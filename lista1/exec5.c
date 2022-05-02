20424f34d2afab8110ecc9f6a0c14d341fb951c5
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-05-02 11:04:40 -0300

    Add files via upload

---
+++ lista1/exec5.c
@@ -0,0 +1,24 @@
+#include<math.h>
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+     int n1,n2,soma;
+
+     printf("entre com dois valores: ");
+     scanf("%d",&n1);
+     scanf("%d",&n2);
+
+
+
+
+     soma = n1 + n2;
+
+     printf("\n a soma dos numeros e:%d",soma);
+
+     getch();
+
+
+}
