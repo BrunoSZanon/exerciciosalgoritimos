056132ca113088d54a57e937b9dac3494a470e6b
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-05-09 08:58:18 -0300

    Add files via upload

---
+++ ditados/ditado1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+ int preco,litro,x;
+
+ printf("insira o valor do litro: ");
+
+ scanf("%d",&litro);
+
+ printf("insira o valor que deseja abastecer: ");
+
+ scanf("%d",&preco);
+
+ x = preco/litro;
+printf("voce abasteceu %d litros de gasolina",x);
+
+}
