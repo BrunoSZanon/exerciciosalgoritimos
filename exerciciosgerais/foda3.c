7c0361df98e157b13b6ff4b4e72aadebd463977e
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-05-30 09:36:39 -0300

    Add files via upload

---
+++ exerciciosgerais/foda3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+main(){
+    int x,y,soma,mult;
+
+    printf("Entre primeiro numero:\n ");
+    scanf("%d",&x);
+
+    printf("Entre segundo numero:\n ");
+    scanf("%d",&y);
+
+    soma = x + y;
+
+    mult = x * soma;
+
+    printf("a multiplicacao e:\n %d",mult);
+
+
+}
