6d33d35cf54d1ab5cf6fea164dc2d95e70e5945b
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-05-30 10:53:18 -0300

    Add files via upload

---
+++ exerciciosgerais/foda6.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+main(){
+
+    float x,y;
+
+    printf("apresente um valor:\n ");
+    scanf("%f",&x);
+
+    printf("apresente outro valor:\n ");
+    scanf("%f",&y);
+
+    if (x>0&&y>0){
+        printf("Numeros sao validos");
+    }
+
+    else {
+        printf("Numeros sao invalidos");
+    }
+
+
+
+
+
+
+}
