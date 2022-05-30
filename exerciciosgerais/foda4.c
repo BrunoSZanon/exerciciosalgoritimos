0822293c8c30dc11517ce54ce061e4807e7d33d7
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-05-30 09:52:37 -0300

    Add files via upload

---
+++ exerciciosgerais/foda4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+main(){
+
+    float c,f;
+
+
+    printf("entre com graus celsius:\n ");
+    scanf("%f",&c);
+
+    f = c * 1.8 + 32;
+
+    printf("seus graus celsius sao:\n  %f",c);
+
+    printf("seus graus fahrenheit:\n %f",f);
+
+
+}
