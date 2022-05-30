71e8f6dcee746dc948901f2e2d4f5dbbaf51f286
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-05-30 09:35:34 -0300

    Add files via upload

---
+++ exerciciosgerais/bagulhomaisfacildetodos.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    int x,y,soma;
+
+    printf("Entre primeiro numero:\n ");
+    scanf("%d",&x);
+
+    printf("Entre segundo numero:\n ");
+    scanf("%d",&y);
+
+    soma = x + y;
+
+    printf("a soma e:\n %d",soma);
+
+}
