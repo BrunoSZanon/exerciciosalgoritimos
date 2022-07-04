4e365ebc2a79f841a6f3977f1f1410347dddd2e6
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-07-04 09:23:32 -0300

    Add files via upload

---
+++ listalaçosderepetição/for.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+main() {
+
+int n, i, soma=0;
+
+printf("entre com um numero:\n ");
+scanf("%d",&n);
+
+for( i=3; i<=n; i++){
+
+   soma=soma + (5*i+2);
+
+}
+
+printf("\n A soma e:%d ", soma);
+
+
+
+
+
+}
