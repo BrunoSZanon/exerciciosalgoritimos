450f941efc408aad9d84c7d670b877cbc9e1fe86
BrunoSZanon <87987250+BrunoSZanon@users.noreply.github.com>
2022-08-08 08:16:27 -0300

    Add files via upload

---
+++ matriz/matriz.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+
+
+main(){
+
+     int i,j,v[3][3];
+
+
+     for(i=0;i<3;i++)
+        for(j=0;j<3;j++){
+
+            printf("\n Entre com os elementos da Matriz: ");
+            scanf("%d",&v[i][j]);
+        }
+
+
+         for(i=0;i<3;i++)
+        for(j=0;j<3;j++){
+
+                printf("\nOs elementos da Matriz:%d",v[i][j]);
+
+        }
+
+
+
+
+
+
+
+
+}
+
